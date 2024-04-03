b2dc28b48979c418a88fad7a25c74d5ad01a36eb
aman-webdev <aman.nabi02@gmail.com>
2024-04-03 23:48:19 +0530

    add end time

--- backend/prisma/migrations/20240403181754_add_endtime_survey/migration.sql
+++ backend/prisma/migrations/20240403181754_add_endtime_survey/migration.sql
@@ -15,6 +15,8 @@ CREATE TABLE "Survey" (
     "description" TEXT,
     "totalParticipants" INTEGER NOT NULL DEFAULT 0,
     "authorId" TEXT NOT NULL,
+    "isAnonymous" BOOLEAN NOT NULL DEFAULT false,
+    "endTime" TIMESTAMP(3),
 
     CONSTRAINT "Survey_pkey" PRIMARY KEY ("id")
 );
@@ -23,6 +25,7 @@ CREATE TABLE "Survey" (
 CREATE TABLE "Question" (
     "id" TEXT NOT NULL,
     "text" TEXT NOT NULL,
+    "surveyId" TEXT NOT NULL,
 
     CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
 );
@@ -31,13 +34,20 @@ CREATE TABLE "Question" (
 CREATE TABLE "Option" (
     "id" TEXT NOT NULL,
     "label" TEXT NOT NULL,
-    "value" INTEGER NOT NULL,
+    "totalVotes" INTEGER NOT NULL DEFAULT 0,
+    "questionId" TEXT NOT NULL,
 
     CONSTRAINT "Option_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "_ParicipatedOn" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_OptionToUser" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL
 );
@@ -54,11 +64,29 @@ CREATE UNIQUE INDEX "_ParicipatedOn_AB_unique" ON "_ParicipatedOn"("A", "B");
 -- CreateIndex
 CREATE INDEX "_ParicipatedOn_B_index" ON "_ParicipatedOn"("B");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "_OptionToUser_AB_unique" ON "_OptionToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_OptionToUser_B_index" ON "_OptionToUser"("B");
+
 -- AddForeignKey
 ALTER TABLE "Survey" ADD CONSTRAINT "Survey_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Question" ADD CONSTRAINT "Question_surveyId_fkey" FOREIGN KEY ("surveyId") REFERENCES "Survey"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Option" ADD CONSTRAINT "Option_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_ParicipatedOn" ADD CONSTRAINT "_ParicipatedOn_A_fkey" FOREIGN KEY ("A") REFERENCES "Survey"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_ParicipatedOn" ADD CONSTRAINT "_ParicipatedOn_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_OptionToUser" ADD CONSTRAINT "_OptionToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Option"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_OptionToUser" ADD CONSTRAINT "_OptionToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
